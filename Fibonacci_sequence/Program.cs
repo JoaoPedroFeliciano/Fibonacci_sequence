fcc5be524eef8a62f3be1e2b0cc3ed6addc72b9d
João Pedro <joao.pedro.guatapara@gmail.com>
2024-09-03 14:32:25 -0300

    Add project files.

---
+++ Fibonacci_sequence/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+class Program
+{
+    static void Main(string[] args)
+
+    {
+        int a = 0;
+        int b = 1;
+        int c;
+
+        Console.Write("Digite um número: ");
+        c = int.Parse(Console.ReadLine());
+
+        while (c > b)
+        {
+            a = a + b;
+            b = b + a;
+        }
+
+        if (c == a || c == b)
+        {
+            Console.WriteLine("O número digitado pertence a sequência de Fibonacci!");
+        }
+        else
+        {
+            Console.WriteLine("O número digitado não pertence a sequência de Fibonacci!");
+        }
+    }
+}
